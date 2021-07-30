c1743fd3f8d35e62e45eb88e28e8eb446efa67b4
sid <sid0308v@gmail.com>
2021-07-30 15:51:57 +0530

    Added cars filter for length greater than 200

--- third.sh
+++ third.sh
@@ -2,3 +2,14 @@
 
 ./second.sh
 ls -l
+
+cat cars.csv  | while read row
+do
+        len=`echo $row | cut -d"," -f2 | tr -d '"'`
+        if [[ $len -gt 200 ]]
+        then
+                echo $row
+        fi
+done
+
+
